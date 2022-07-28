3678dd2e13b301bac424fbc8d466f7ac0bfc4323
DLindeblom <drdlindeblom@gmail.com>
2022-07-28 14:53:55 +0000

    added validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validation :name, length: (minimum: 3)
+
+  validation :annual_revenue, numericality: (greater_than: 0)
+
+  validate :must_sell_one_type_of_clothing
+    def must_sell_one_type_of_clothing
+      if mens_apparel == false && womens_apparel == false
+        errors.add("You have to seel one type of apparel")
+      end
+    end
+    
 end
